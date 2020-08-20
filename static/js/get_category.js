5813c883e6b93145334c8d452f00200a5377b11d
yuancong@yunify.com <19890912Cc!>
2020-08-20 10:44:24 +0800

    init

---
+++ static/js/get_category.js
@@ -0,0 +1,42 @@
+$(function(){
+	$.getJSON("category.json", function (data){
+		var content = '';
+		$.each(data,function(key,value){
+			//二级目录
+			if(key=='1'){
+				var child_category = '<div class="card-content tab-panel is-active"><div class="columns is-multiline is-gapless">';
+			}else{
+				var child_category = '<div class="card-content tab-panel" style="display: none;"><div class="columns is-multiline is-gapless">';
+			}
+			
+			$.each(value.category,function(k,v){
+				child_category += '<div class="column is-4"><a class="card" href="/product_index/index.html?id='+key+'_'+k+'" target="_blank"><h5 class="title">'+v.name+'</h5><p class="is-text-small is-text-gray">'+v.content+'</p></a></div>';
+			});
+			child_category += '</div></div>';
+			//一级目录
+			if(key=='1'){
+				content += '<li class="is-active"><a>'+value.name+'</a></li>';
+			}else{
+				content += '<li><a>'+value.name+'</a></li>';
+			}
+			$('#child_content').append(child_category);
+		});
+		$('#ul_content').html(content);
+		tabs($('.price .subtabs'), $('.price .subtab-content'), 'click');
+	});
+	
+});
+
+function tabs(tabMenu, tabContent, event, activeClass, index) {
+    activeClass = activeClass || 'is-active'
+    index = parseInt(index) || 0
+    tabMenu.find('> ul > li').eq(index).addClass(activeClass)
+    tabContent.find('> .tab-panel').eq(index).addClass(activeClass)
+    tabMenu.find('> ul > li').on(event || 'click', function (e) {
+        $(this).addClass(activeClass).siblings().removeClass(activeClass);
+        var index = tabMenu.find('> ul > li').index(this);
+        var currTabContent = tabContent.find('> .tab-panel').eq(index);
+        currTabContent.show().siblings().removeClass(activeClass).hide();
+        currTabContent.addClass(activeClass);
+    });
+};
